ecefa8fb1f7e5b4421010c28d21c22e99c9af72a
Qasid Labeed <qasidlabeed@gmail.com>
2025-06-02 22:59:02 +0100

    Add logic for fetching and setting a static riddle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,18 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
+import riddleArtifact from "../../hardhat/artifacts/contracts/Riddle.sol/OnchainRiddle.json";
+
+export const CONTRACT_ABI = riddleArtifact.abi;
+export const CONTRACT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3"; // Replace with your contract address
+
 export default function Riddle() {
   const [signer, setSigner] = useState<ethers.Signer | null>(null);
-  const [provider, setProvider] = useState<ethers.BrowserProvider | null>(null);
+  const [provider, setProvider] = useState<ethers.JsonRpcApiProvider | null>(
+    null
+  );
+  const [riddle, setRidle] = useState<string | null>(null);
+  const [contract, setContract] = useState<ethers.Contract | null>(null);
 
   useEffect(() => {
     //Connect to wallet
@@ -12,23 +21,66 @@ export default function Riddle() {
   }, []);
 
   const connectWallet = async () => {
-    if (!window.ethereum) {
-      alert("MetaMask is required to play the game. Please install it.");
-      return;
-    }
 
     try {
-
-      //Provider to the blockchain
-      const _provider = new ethers.BrowserProvider(window.ethereum);
+      const _provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
       setProvider(_provider);
-
-     //Signer to sign the transactions
-      const _signer = await _provider.getSigner();
+      // Get first account from local node (unlocked by default)
+      const _signer = await _provider.getSigner(0);
       setSigner(_signer);
-      
+
+      const _contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI,
+        _signer
+      );
+       setContract(_contract);
+
+      console.log("Connected to contract: " + _contract?.runner.address);
+
+
+      if (_contract) {
+        // Load contract data
+        loadContractData(_contract);
+      }
+
     } catch (err) {
       console.error("Wallet connection failed:", err);
+    }
+  }
+  /**
+   * Loads the contract data and fetches the current riddle.
+   *
+   **/
+  const loadContractData = async (contractInstance: ethers.Contract) => {
+    try {
+      const riddle = await contractInstance.riddle();
+
+      if (!riddle) {
+        setRiddle(contractInstance, "Question", "Answer");
+      }
+      console.log("Current Riddle:", riddle);
+      setRidle(riddle);
+      // Update state or UI with the riddle
+    } catch (err) {
+      console.error("Failed to fetch riddle:", err);
+    }
+  };
+
+  const setRiddle = async (
+    contractInstance: ethers.Contract,
+    question: string,
+    answer: string
+  ) => {
+    try {
+      const answerHash = ethers.keccak256(ethers.toUtf8Bytes(answer));
+
+      const tx = await contractInstance.setRiddle(question, answerHash);
+      await tx.wait();
+      console.log("Riddle set successfully");
+
+    } catch (err) {
+      console.error("Failed to set riddle:", err);
     }
   };
 
@@ -39,7 +91,7 @@ export default function Riddle() {
 
         <div className="bg-gray-700 rounded-lg p-4 mb-6">
           <h2 className="text-lg font-semibold mb-2">Current Riddle:</h2>
-          <p className="text-gray-300 italic">{"Riddle Question"}</p>
+          <p className="text-gray-300 italic">{"Riddle Question" + riddle}</p>
         </div>
 
         <div className="space-y-3">
